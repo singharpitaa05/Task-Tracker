7633eb6acda917754eae3f20c66728bac0e29265
singharpita05 <singharpita.05march@gmail.com>
2026-01-13 13:39:45 +0530

    INITIAL SET UP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 const App = () => {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline"> Task tracker</h1>
+      <h1 class="text-3xl font-bold underline"> </h1>
     </div>
   )
 }
